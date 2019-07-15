112a38d48b93fba09668a519af44ec519694595b
QaziAnwar <qazi.skill60@gmail.com>
2019-07-15 10:54:55 +0500

    TestCalculator

---
+++ src/Calculation.java
@@ -0,0 +1,22 @@
+package Final;
+
+public class Calculation {
+
+	
+	
+	public void add()
+	{
+		int a=6;
+		int b=9;
+		
+		int add=a+b;
+		
+		
+		System.out.println("Addation method"+add);
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/site/TestCalculator.java
@@ -0,0 +1,22 @@
+package Final;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalculator {
+
+	@Test
+	void findMax() {
+		
+		Calculator cal=new Calculator();
+		
+		
+	}
+
+	@Test
+	void square()
+	{
+		
+	}
+}
