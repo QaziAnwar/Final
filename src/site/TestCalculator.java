24d2902ba966adc4812e74ae1d09c75ec819b05f
QaziAnwar <qazi.skill60@gmail.com>
2019-07-15 11:03:19 +0500

    TestCalculator Class

--- src/site/TestCalculator.java
+++ src/site/TestCalculator.java
@@ -1,5 +1,6 @@
 package Final;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,20 @@ class TestCalculator {
 		
 		Calculator cal=new Calculator();
 		
+		get
+		int result=test.add(2, 3,3);
+		
+		assertEquals(actual,result);
 		
 	}
 
 	@Test
 	void square()
 	{
+Calculator cal=new Calculator();
 		
+		int result=test.add(6);
+		
+		assertEquals(14,result);
 	}
 }
